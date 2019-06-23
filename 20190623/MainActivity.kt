e881342fd51136cc75609ad51e0bef436a29841e
hoonkk <byul2sol@naver.com>
2019-06-23 22:41:06 +0900

    20190623 Work

---
+++ 20190623/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.bmicalculator
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultButton.setOnClickListener {
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString() ,
+                "height" to heightEditText.text.toString())
+        }
+    }
+}
