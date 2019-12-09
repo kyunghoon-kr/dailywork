1400dcf551060eab534131e6b9e087ec3dab38de
kimkyunghoon <byul2sol@naver.com>
2019-12-09 15:31:02 +0900

    Sass 라이브러리 사용하기?

---
+++ By Content/React/컴포넌트 스타일링/Sass/SassComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SassComponent.scss';
+const SassComponent = (props) => {
+    return (
+        <div>
+            <div className="SassComponent">
+                <div className="box red"/>
+            </div>
+        </div>
+    );
+};
+
+export default SassComponent;
