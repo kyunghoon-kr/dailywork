7c2b78b5586a04f3267aadffff5a82c14486f536
kimkyunghoon <byul2sol@naver.com>
2019-12-24 15:55:56 +0900

    reactstrap 적용해보기

---
+++ By Content/React/reactstrap/적용하기/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css'
+import { Button } from 'reactstrap'
+function App() {
+  return (
+    <div>
+      <Button color="primary">primary</Button>{''}
+    </div>
+  );
+}
+
+export default App;
