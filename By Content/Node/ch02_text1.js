59d80fdb177ba451a7a540da691417aa6df063c3
kimkyunghoon <byul2sol@naver.com>
2020-01-19 20:37:01 +0900

    Nodejs 시작하기

---
+++ By Content/Node/ch02_text1.js
@@ -0,0 +1,11 @@
+var result = 0;
+console.time('duration_sum');
+
+for (var i = 1; i<= 1000; i ++) {
+    result += i;
+}
+
+console.timeEnd('duration_sum');
+console.log('1부터 1000까지 더한 결과물 : %d' , result);
+console.log('%s', __filename);
+console.log('%s', __dirname);
