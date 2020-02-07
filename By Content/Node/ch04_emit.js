360ee897e28830b9ac663e0c454b51a68d2ae68e
kimkyunghoon <byul2sol@naver.com>
2020-02-07 21:54:09 +0900

    Node: 이벤트 처리

---
+++ By Content/Node/ch04_emit.js
@@ -0,0 +1,11 @@
+process.on('tick', function(count) {
+    console.log('tick 이벤트 발생함 : %s', count)
+});
+
+setTimeout(function() {
+    console.log('2초 후에 tick 이벤트 전달 시도');
+
+    process.emit('tick', '2');
+},2000);
+
+// emit 메소드를 통하여 이벤트를 다른 곳으로 전달.
---
+++ By Content/Node/ch04_exitEvent.js
@@ -0,0 +1,11 @@
+process.on('exit', function() {
+    console.log('exit 이벤트 발생함');
+});
+
+setTimeout(function () {
+    console.log('2초 후에 시스템 종료 시도함.');
+    
+    process.exit();
+}, 2000);
+
+// process의 on 메소드를 호출
